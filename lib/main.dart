9a0f77f766df87f5c50fb8aa8a9dde565d8414f8
jonascolas <jonas@fusemachines.com>
2022-02-25 20:12:11 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hotels Booking',
+      home: Scaffold(
+          appBar: AppBar(
+        title: Text('Home'),
+      )),
+    );
+  }
+}
